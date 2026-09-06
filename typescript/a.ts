81205aed4fb35bdb2cacfd32bb85055d93881535
JGW-Korea <jgw6372@gmail.com>
2026-09-06 22:57:35 +0900

    typescript 확인차 수정

---
+++ typescript/a.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+let a = 10;
+
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+import "./b";
+
+console.log("A1")
+console.log("A2")
+console.log("A3")
+console.log("A4")
---
+++ typescript/b.ts
@@ -0,0 +1,9 @@
+let a = 10
+
+console.log("B1")
+console.log("B2")
+console.log("B3")
+console.log("B4")
+console.log("B5")
+
+export default a
